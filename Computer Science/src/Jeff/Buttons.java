b43f5e40f6042ece60da3634f0fc8a5dfeacef63
jeffpark <jeffpark@192.168.0.149>
2016-12-15 08:49:44 +0900

    Final commit

---
+++ Computer Science/src/Jeff/Buttons.java
@@ -0,0 +1,13 @@
+package Jeff;
+
+import java.util.Scanner;
+public class Buttons {
+	public static void main (String[]args){
+		Scanner buttons = new Scanner (System.in);
+		int button1, button2, button3;
+		System.out.print("Click the button");
+		
+		
+	}
+
+}
